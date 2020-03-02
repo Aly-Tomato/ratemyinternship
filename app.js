99d35dc572ac4540eadae25eb10daa7994963da4
Robert Pelayo <robpelayo@gmail.com>
2020-03-02 04:27:10 -0800

    Got ajax working with dummy database code

--- app.js
+++ app.js
@@ -31,5 +31,28 @@ app.get('/thankyou', addReview.thankyou);
 app.get('/admin', admin.login);
 app.post('/admin', admin.auth);
 
+// testing ajax
+const testDataBase = {
+    '0': {company: 'Intel', city: 'Portland', position: 'Software Engineer', start: '2003-12-02', end: '2004-04-01', intern_type: 'other', pay_type: 'Annual', pay_amount: '$20,000 - $35,000', work_desc: 'Engineering some programs.', overall_rating: '5', leadership_rating: '4', communication_rating: '3', flexibility_rating: '2', culture_rating: '1', internship_recommend: '2', company_recommend: '3', ratings_desc: 'Theyre ok.'},
+    '1': {company: 'Nike', city: 'Beaverton', position: 'Software Engineering Intern', start: '2018-12-02', end: '2020-04-01', intern_type: 'other', pay_type: 'Annual', pay_amount: '$20,000 - $35,000', work_desc: 'Engineering some high tech shoes', overall_rating: '5', leadership_rating: '4', communication_rating: '3', flexibility_rating: '2', culture_rating: '1', internship_recommend: '2', company_recommend: '3', ratings_desc: 'NULL'}
+};
+
+app.get('/test', (req, res) => {
+    const allUsers = Object.keys(testDataBase);
+    res.send(allUsers);
+})
+
+app.get('/test/:userid', (req, res) => {
+    const nameToLookup = req.params.userid;
+    const val = testDataBase[nameToLookup];
+    console.log(nameToLookup, '->', val);
+    if (val) {
+        res.send(val);
+    } else {
+        res.send({});
+    }
+})
+
+
 app.listen(5000);
 console.log(`Listening on localhost:5000`);
--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,58 @@
-function queryDB(){
-    document.getElementById("response").innerHTML = res.rows;
-}
+$(document).ready(() => {
+    $('#submitButton').click(() => {
+        const requestURL = 'test/' + $('#searchBox').val();
+        console.log('making ajax request to:', requestURL);
+        $.ajax({
+            url: requestURL,
+            type: 'GET',
+            dataType: 'json',
+            success: (data) =>{
+                console.log('ajax success!', JSON.stringify(data, null, 8));
+                // for (var i = 0; i < data.length; ++i){
+                //     currentData = data[i];
+                //     console.log(
+                //         '{' + '\n' + 
+                //         '       company: ' + JSON.stringify(currentData.company) + "\n" +
+                //         '       city: ' + JSON.stringify(currentData.city) + "\n" +
+                //         '       position: ' + JSON.stringify(currentData.position) + "\n" +
+                //         '       start: ' + JSON.stringify(currentData.start) + "\n" +
+                //         '       end: ' + JSON.stringify(currentData.end) + "\n" +
+                //         '       intern_type: ' + JSON.stringify(currentData.intern_type) + "\n" +
+                //         '       pay_type: ' + JSON.stringify(currentData.pay_type) + "\n" +
+                //         '       pay_amount: ' + JSON.stringify(currentData.pay_amount) + "\n" +
+                //         '       work_desc: ' + JSON.stringify(currentData.work_desc) + "\n" +
+                //         '       overall_rating: ' + JSON.stringify(currentData.overall_rating) + "\n" +
+                //         '       leadership_rating: ' + JSON.stringify(currentData.leadership_rating) + "\n" +
+                //         '       communication_rating: ' + JSON.stringify(currentData.communication_rating) + "\n" +
+                //         '       flexibility_rating: ' + JSON.stringify(currentData.flexibility_rating) + "\n" +
+                //         '       culture_rating: ' + JSON.stringify(currentData.culture_rating) + "\n" +
+                //         '       internship_recommend: ' + JSON.stringify(currentData.internship_recommend) + "\n" +
+                //         '       company_recommend: ' + JSON.stringify(currentData.company_recommend) + "\n" +
+                //         '       ratings_desc: ' + JSON.stringify(currentData.ratings_desc) + "\n" +
+                //         '}'); 
+                // }
+                $('#response').html(JSON.stringify(data, null, 8));
+                // $('#response').html(
+                //     '{' + '\n' + 
+                //     '       company: ' + JSON.stringify(data.company) + "\n" +
+                //     '       city: ' + JSON.stringify(data.city) + "\n" +
+                //     '       position: ' + JSON.stringify(data.position) + "\n" +
+                //     '       start: ' + JSON.stringify(data.start) + "\n" +
+                //     '       end: ' + JSON.stringify(data.end) + "\n" +
+                //     '       intern_type: ' + JSON.stringify(data.intern_type) + "\n" +
+                //     '       pay_type: ' + JSON.stringify(data.pay_type) + "\n" +
+                //     '       pay_amount: ' + JSON.stringify(data.pay_amount) + "\n" +
+                //     '       work_desc: ' + JSON.stringify(data.work_desc) + "\n" +
+                //     '       overall_rating: ' + JSON.stringify(data.overall_rating) + "\n" +
+                //     '       leadership_rating: ' + JSON.stringify(data.leadership_rating) + "\n" +
+                //     '       communication_rating: ' + JSON.stringify(data.communication_rating) + "\n" +
+                //     '       flexibility_rating: ' + JSON.stringify(data.flexibility_rating) + "\n" +
+                //     '       culture_rating: ' + JSON.stringify(data.culture_rating) + "\n" +
+                //     '       internship_recommend: ' + JSON.stringify(data.internship_recommend) + "\n" +
+                //     '       company_recommend: ' + JSON.stringify(data.company_recommend) + "\n" +
+                //     '       ratings_desc: ' + JSON.stringify(data.ratings_desc) + "\n" +
+                //     '}');
+            }
+        });
+    })
+})
