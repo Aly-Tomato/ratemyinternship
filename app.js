7effd5289a656857128f615651657d3f4870f785
Robert Pelayo <robpelayo@gmail.com>
2020-03-02 13:21:13 -0800

    Got all companies going to the text area, need to work on search

--- app.js
+++ app.js
@@ -31,28 +31,22 @@ app.get('/thankyou', addReview.thankyou);
 app.get('/admin', admin.login);
 app.post('/admin', admin.auth);
 
-// testing ajax
-const testDataBase = {
-    '0': {company: 'Intel', city: 'Portland', position: 'Software Engineer', start: '2003-12-02', end: '2004-04-01', intern_type: 'other', pay_type: 'Annual', pay_amount: '$20,000 - $35,000', work_desc: 'Engineering some programs.', overall_rating: '5', leadership_rating: '4', communication_rating: '3', flexibility_rating: '2', culture_rating: '1', internship_recommend: '2', company_recommend: '3', ratings_desc: 'Theyre ok.'},
-    '1': {company: 'Nike', city: 'Beaverton', position: 'Software Engineering Intern', start: '2018-12-02', end: '2020-04-01', intern_type: 'other', pay_type: 'Annual', pay_amount: '$20,000 - $35,000', work_desc: 'Engineering some high tech shoes', overall_rating: '5', leadership_rating: '4', communication_rating: '3', flexibility_rating: '2', culture_rating: '1', internship_recommend: '2', company_recommend: '3', ratings_desc: 'NULL'}
-};
 
-app.get('/test', (req, res) => {
-    const allUsers = Object.keys(testDataBase);
-    res.send(allUsers);
+app.get('/all', (req, res) => {
+    const { Pool } = require("pg");
+    const pool = new Pool({
+        user: "atamayo",
+        host: "db.cecs.pdx.edu",
+        database: "atamayo",
+        password: "y4@z4qxgMw",
+        port: 5432
+    });
+
+    pool.query("SELECT* FROM reviews", (err, result) => {
+        if(err) throw err;
+        res.send(result.rows);
+    });
 })
-
-app.get('/test/:userid', (req, res) => {
-    const nameToLookup = req.params.userid;
-    const val = testDataBase[nameToLookup];
-    console.log(nameToLookup, '->', val);
-    if (val) {
-        res.send(val);
-    } else {
-        res.send({});
-    }
-})
-
 
 app.listen(5000);
 console.log(`Listening on localhost:5000`);
--- routes/index.js
+++ routes/index.js
@@ -12,8 +12,7 @@ exports.getURL = function(req, res) {
         port: 5432
     });
 
-    var rows = pool.query("SELECT* FROM views", (err, res) => {
-        console.log(res.rows);
+    var rows = pool.query("SELECT* FROM reviews", (err, res) => {
         if(err) throw err;
         return res.rows;
     });
@@ -25,6 +24,53 @@ exports.getURL = function(req, res) {
        var elem = document.getElementById('response').innerText;
        console.log(elem);
     });
-
-   res.send();
+    console.log(res.rows);
+   res.send(rows);
 };
+// app.get('/test', (req, res) => {
+//     // const allUsers = Object.keys(testDataBase);
+//     // res.send(allUsers);
+//     console.log('Entered /test');
+//     const { Pool } = require("pg");
+//     const pool = new Pool({
+//         user: "atamayo",
+//         host: "db.cecs.pdx.edu",
+//         database: "atamayo",
+//         password: "y4@z4qxgMw",
+//         port: 5432
+//     });
+
+//     pool.query("SELECT* FROM reviews", (err, result) => {
+//         if(err) throw err;
+//         // console.log(result.rows);
+//         // const allCompanies = result.rows.map(e => e.company);
+//         // console.log(allCompanies);
+//         res.send(result.rows);
+//     });
+// })
+
+// app.get('/test/:intern_typeid', (req, res) => {
+//     const companyToLookup = req.params.intern_typeid;
+//     console.log('Entered /test/companyid');
+//     const { Pool } = require("pg");
+//     const pool = new Pool({
+//         user: "atamayo",
+//         host: "db.cecs.pdx.edu",
+//         database: "atamayo",
+//         password: "y4@z4qxgMw",
+//         port: 5432
+//     });
+//     {
+//       $intern_type: companyToLookup
+//     }
+
+//     pool.query("SELECT* FROM reviews WHERE intern_type=$intern_type", (err, result) => {
+//         if(err) throw err;
+//         console.log(result.rows);
+//     }); 
+//     // if (val) {
+//     //     res.send(val);
+//     // } else {
+//     //     res.send({});
+//     // }
+// })
