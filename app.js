d6347d1eefcb74cfe97fb00b927d89ce6fce5e97
Robert Pelayo <robpelayo@gmail.com>
2020-03-07 13:55:20 -0800

    Server error handling

--- app.js
+++ app.js
@@ -1,60 +1,80 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use('views/', express.static('views'));
-app.use('/static', express.static('static'));
-app.use(express.urlencoded({'extended': false}));
+app.use("views/", express.static("views"));
+app.use("/static", express.static("static"));
+app.use(express.urlencoded({ extended: false }));
 
 const { Pool } = require("pg");
 const pool = new Pool();
 
-var documentation = require('./routes/documentation');
-var addReview = require('./routes/addReview');
-var admin = require('./routes/admin');
-var index = require('./routes/index');
+var documentation = require("./routes/documentation");
+var addReview = require("./routes/addReview");
+var admin = require("./routes/admin");
+var index = require("./routes/index");
 
 module.exports = app;
 
 // initial index route
-app.get('/', (req, res) =>{
-    res.status(200);
-    res.sendFile('index.html', {root: 'views/'});
+app.get("/", (req, res) => {
+  res.status(200);
+  res.sendFile("index.html", { root: "views/" });
 });
-app.post('/getURL', index.getURL);
+app.post("/getURL", index.getURL);
 
 // documentation routes
-app.get('/documentation', documentation.list);
+app.get("/documentation", documentation.list);
 
 // addReview routes
-app.get('/addReview', addReview.form);
-app.post('/submit', addReview.submitReview);
-app.get('/thankyou', addReview.thankyou);
+app.get("/addReview", addReview.form);
+app.post("/submit", addReview.submitReview);
+app.get("/thankyou", addReview.thankyou);
 
 // admin routes
-app.get('/admin', admin.login);
-app.post('/admin', admin.auth);
+app.get("/admin", admin.login);
+app.post("/admin", admin.auth);
 
-app.get('/api/:field/:value', (req, res) =>{
-    var query = `SELECT * FROM reviews WHERE ${req.params.field}='${req.params.value}'`;
-    console.log(query);
-    pool.query(`SELECT * FROM reviews WHERE ${req.params.field}='${req.params.value}'`, (err, result) => {
-        if(err) throw err;
+app.get("/api/:field/:value", (req, res) => {
+  var query = `SELECT * FROM reviews WHERE ${req.params.field}='${req.params.value}'`;
+  pool.query(
+    `SELECT * FROM reviews WHERE ${req.params.field}='${req.params.value}'`,
+    (err, result) => {
+      try {
+        if (err) throw err;
         res.send(result.rows);
-    });
+      } catch {
+        res.status(500);
+        res.send("Error");
+      }
+    }
+  );
 });
 
-app.get('/api/all/:field', (req, res) =>{
-    pool.query(`SELECT DISTINCT(${req.params.field}) FROM reviews`, (err, result) => {
-        if(err) throw err;
+app.get("/api/all/:field", (req, res) => {
+  pool.query(
+    `SELECT DISTINCT(${req.params.field}) FROM reviews`,
+    (err, result) => {
+      try {
+        if (err) throw err;
         res.send(result.rows);
-    });
+      } catch {
+        res.status(500);
+        res.send("Error");
+      }
+    }
+  );
 });
 
-app.get('/api/all', (req, res) => {
-    pool.query("SELECT* FROM reviews", (err, result) => {
-        if(err) throw err;
-        res.send(result.rows);
-    });
+app.get("/api/all", (req, res) => {
+  pool.query("SELECT* FROM reviews", (err, result) => {
+    try {
+      if (err) throw err;
+      res.send(result.rows);
+    } catch {
+        res.status(500);
+        res.send("Error");
+    }
+  });
 });
 
 app.listen(5000);
--- static/js/index.js
+++ static/js/index.js
@@ -1,23 +1,27 @@
 $(document).ready(() => {
-    $('#submitButton').click(() => {
-        const requestURL = 'api/' + $('#searchBox').val();
-        console.log('making ajax request to:', requestURL);
-        $.ajax({
-            url: requestURL,
-            type: 'GET',
-            dataType: 'json',
-            success: (data) =>{
-                console.log('ajax success!', JSON.stringify(data, null, 8));
-                $('#response').html(JSON.stringify(data, null, 8));
-            }
-        });
-    })
+  $("#submitButton").click(() => {
+    const requestURL = "api/" + $("#searchBox").val();
+    $.ajax({
+      url: requestURL,
+      type: "GET",
+      dataType: "json",
+      success: data => {
+        console.log("ajax success!", JSON.stringify(data, null, 8));
+        $("#response").html(JSON.stringify(data, null, 8));
+      },
+      error: function(err) {
+        $("#response").html(
+          "Could not find any resource that matches the request. Please read the documentation."
+        );
+      }
+    });
+  });
 });
 
 var input = document.getElementById("searchBox");
 input.addEventListener("keyup", function(event) {
   if (event.keyCode === 13) {
-   event.preventDefault();
-   document.getElementById("submitButton").click();
+    event.preventDefault();
+    document.getElementById("submitButton").click();
   }
 });
